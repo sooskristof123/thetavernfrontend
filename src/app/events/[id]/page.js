d26572e6b06f0a26c79cea25f679ddd6bb29ea44
Kristof Soos <sooskristof123@gmail.com>
2025-09-27 19:00:37 +0200

    feat: added event's header and banner

--- src/app/events/[id]/page.js
+++ src/app/events/[id]/page.js
@@ -1,7 +1,20 @@
+import Button from "@/app/components/button";
 import styles from "./events.module.css";
 
 export default function Events() {
   return (
-    <div></div>
+    <div className={styles.page}>
+      <div className={styles.page_event_banner}>
+        <div className={styles.page_event_banner__overlay}></div>
+        <div className={styles.page_event_banner__overlay2}></div>
+      </div>
+
+      <div className={styles.page_content}>
+        <div className={styles.page_content__header}>
+          <h2>Magyar Streamer Bajnokság</h2>
+          <Button animated={true} variant="secondary" label="Jegyvásárlás"/>
+        </div>
+      </div>
+    </div>
   );
 }
