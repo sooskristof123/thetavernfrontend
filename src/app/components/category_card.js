0cb5a3e25f0c7a6a7479230506ddf12218d9585f
Kristof Soos <sooskristof123@gmail.com>
2025-09-29 00:09:03 +0200

    feat: added enter animation on category cards

--- src/app/components/category_card.js
+++ src/app/components/category_card.js
@@ -1,13 +1,25 @@
+"use client";
 import Image from "next/image";
 import styles from "./category_card.module.css";
+import { motion } from "framer-motion";
 
-export default function CategoryCard() {
+export default function CategoryCard(props) {
   return (
-    <div className={styles.category_card}>
+    <motion.div 
+      className={styles.category_card}
+      initial={{ opacity: 0, rotateY: 45 }}
+      whileInView={{ opacity: 1,  rotateY: 0 }}
+      viewport={{ once: true, amount: 0.3 }} 
+      transition={{
+            duration: 0.7,
+            delay: props.index * 0.1,
+            type: "spring",
+      }}
+    >
       <Image className={styles.category_card__icon} src="/icons/trophy_icon.svg" alt="Trophy Icon" width={65} height={65} />
       <div className={styles.category_card__content}>
         <p className={styles.category_card__subtitle}>Verseny</p>
       </div>
-    </div>
+    </motion.div>
   );
 }
