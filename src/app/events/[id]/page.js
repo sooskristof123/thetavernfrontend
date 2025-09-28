27d21224dfd0bf6e9ad451b2dd4d3aaa0beb26eb
Dzsigi <ulicsyn.kristof@gmail.com>
2025-09-28 17:07:40 +0200

    feat: added nice animations to main and event page

--- src/app/events/[id]/page.js
+++ src/app/events/[id]/page.js
@@ -68,6 +68,38 @@ export default function Events() {
           <LocationCard location="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8435.365785041067!2d21.619547750910066!3d47.55504320720335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47470df39a92233b%3A0x158b32cc387080a5!2sDebreceni%20Egyetem!5e0!3m2!1shu!2shu!4v1759054267093!5m2!1shu!2shu" />
         </div>
         <div className={styles.page_content__events_near}>
+          <div className={styles.particles}>
+            <span style={{ '--seed': 0.12, '--x': '10%' }}></span>
+            <span style={{ '--seed': 0.73, '--x': '2%' }}></span>
+            <span style={{ '--seed': 0.41, '--x': '14%' }}></span>
+            <span style={{ '--seed': 0.09, '--x': '56%' }}></span>
+            <span style={{ '--seed': 0.56, '--x': '63%' }}></span>
+            <span style={{ '--seed': 0.27, '--x': '72%' }}></span>
+            <span style={{ '--seed': 0.84, '--x': '22%' }}></span>
+            <span style={{ '--seed': 0.33, '--x': '8%' }}></span>
+            <span style={{ '--seed': 0.61, '--x': '82%' }}></span>
+            <span style={{ '--seed': 0.18, '--x': '54%' }}></span>
+            <span style={{ '--seed': 0.95, '--x': '76%' }}></span>
+            <span style={{ '--seed': 0.44, '--x': '24%' }}></span>
+            <span style={{ '--seed': 0.70, '--x': '35%' }}></span>
+            <span style={{ '--seed': 0.05, '--x': '78%' }}></span>
+            <span style={{ '--seed': 0.39, '--x': '66%' }}></span>
+            <span style={{ '--seed': 0.81, '--x': '84%' }}></span>
+            <span style={{ '--seed': 0.24, '--x': '25%' }}></span>
+            <span style={{ '--seed': 0.66, '--x': '29%' }}></span>
+            <span style={{ '--seed': 0.13, '--x': '72%' }}></span>
+            <span style={{ '--seed': 0.52, '--x': '24%' }}></span>
+            <span style={{ '--seed': 0.37, '--x': '15%' }}></span>
+            <span style={{ '--seed': 0.88, '--x': '19%' }}></span>
+            <span style={{ '--seed': 0.46, '--x': '62%' }}></span>
+            <span style={{ '--seed': 0.30, '--x': '47%' }}></span>
+            <span style={{ '--seed': 0.74, '--x': '81%' }}></span>
+            <span style={{ '--seed': 0.19, '--x': '90%' }}></span>
+            <span style={{ '--seed': 0.63, '--x': '5%' }}></span>
+            <span style={{ '--seed': 0.07, '--x': '50%' }}></span>
+            <span style={{ '--seed': 0.51, '--x': '43%' }}></span>
+            <span style={{ '--seed': 0.96, '--x': '23%' }}></span>
+          </div>
           <h4>További hasonló esemény</h4>
           <EventsNear />
         </div>
