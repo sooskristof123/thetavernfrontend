a3f4cab7cb1d5245d8642b18af5358353a952922
Kristof Soos <sooskristof123@gmail.com>
2025-10-03 01:56:52 +0200

    feat: added profile icon and login mocking in header

---
+++ src/app/components/profile_icon.js
@@ -0,0 +1,10 @@
+import styles from "./profile_icon.module.css";
+
+export default function ProfileIcon() {
+
+  return (
+    <div className={styles.profile_icon}>
+      
+    </div>
+  );
+}
--- src/app/sections/header.js
+++ src/app/sections/header.js
@@ -5,12 +5,19 @@ import Button from "../components/button";
 import Navigation from "../components/navigation";
 import SearchBar from "../components/searchbar";
 import styles from "./header.module.css";
+import ProfileIcon from "../components/profile_icon";
 
 export default function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  // mock login state
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false);
 
   const handleMobileMenuClick = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
+  }
+
+  const handleLoginClick = () => {
+    setIsLoggedIn(!isLoggedIn);
   }
 
   return (
@@ -23,19 +30,23 @@ export default function Header() {
           <Image onClick={handleMobileMenuClick} className={styles.header_mobile__nav__close} src="/icons/close.svg" alt="Close" width={24} height={24} />
         }
       </div>
-      <SearchBar />
+      <div className={styles.header_searchbar__wrapper}>
+        <SearchBar />
+      </div>
       <Navigation />
-      <Button label="Bejelentkezés"/>
-      { isMobileMenuOpen ? 
-        <div className={styles.header_mobile__nav__content}>
-          <Navigation isMobile={true} />
-          <div className={styles.header_mobile__nav__content__button}>
-            <Button label="Bejelentkezés" />
+      { isLoggedIn ? 
+        <ProfileIcon />
+        : 
+        isMobileMenuOpen ? 
+          <div className={styles.header_mobile__nav__content}>
+            <Navigation isMobile={true} />
+            <div className={styles.header_mobile__nav__content__button}>
+              <Button label="Bejelentkezés" onClick={handleLoginClick} />
+            </div>
+            <p>Kövess minket!</p>
           </div>
-          <p>Kövess minket!</p>
-        </div>
-        :
-        <></>
+          :
+          <Button label="Bejelentkezés" onClick={handleLoginClick} />
       }
     </div>
   );
